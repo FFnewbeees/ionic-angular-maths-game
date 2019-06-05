1268eb88203e11d6d3cdebd20091787148199cb3
Victor Liu <victorliu@Victors-MacBook-Pro.local>
2019-06-05 20:29:28 +1000

    added timer and some logics

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { questionItem } from '../models/question.model';
-import { HttpClient } from '@angular/common/http';
+
 
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  
 })
 export class HomePage implements OnInit{
 
-  apiUrl = 'https://math.ly/api/v1/algebra/linear-equations.json';
+  score:number = 0;
+  time:number = 60;
+  questionNumber:number = 1;
+  interval;
+  started:boolean = false;
+  userInput:string;
+  stop:boolean = false;
+  correct:boolean = false;
 
   question:questionItem = {
     firstNumber:0,
@@ -18,23 +26,71 @@ export class HomePage implements OnInit{
     answer: 0
   };
 
-  constructor(private http:HttpClient) {
+  constructor() {
+    
   }
 
   ngOnInit(){
-    this.question = {
-      firstNumber:1,
-      secondNumber:2,
-      answer: this.question.firstNumber + this.question.secondNumber
-    }
-
-    this.loadQuestion();
+    
   }
 
-  loadQuestion(){
-    return this.http.get(this.apiUrl);
+  startGame(){
+    this.interval = setInterval(()=> {
+      if(this.time > 0){
+        this.time--;
+      }
+      else{
+        this.time = 0;
+        this.stop = true;
+        this.started = false;
+      }
+    } ,1000)
+
+    this.started = true;
+
+    this.generateQuestion();
+  }
+
+  stopGame(){
+    clearInterval(this.interval);
+    this.stop = true;
     
-    
+  }
+
+  generateQuestion(){
+    this.question.firstNumber = Math.floor(Math.random() * 100) + 1;
+    this.question.secondNumber = Math.floor(Math.random() * 100) + 1;
+    this.question.answer = this.question.firstNumber + this.question.secondNumber;
+    this.correct = false;
+    console.log(this.question.answer);
+
+  }
+
+  checkAnswer(){
+    if(parseInt(this.userInput) == this.question.answer)
+    {
+      this.score += 5;
+
+      this.correct = true;
+
+      this.userInput = "";
+
+
+
+      if(this.time > 0){
+        this.generateQuestion();
+        this.questionNumber++;
+      }
+      else{
+        //end game
+        this.stop = false;
+      }
+
+      
+      
+
+    }
+  
   }
 
 
